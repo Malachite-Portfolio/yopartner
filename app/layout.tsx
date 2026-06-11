aec6452fdbe726f9a7572ba58e45efae3c7d4b6e
Ashish Kumar <ashishkumarasd698@gmail.com>
2026-06-11 11:11:41 +0530

    Add global Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={manrope.variable}>
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8PMNJLDEZP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8PMNJLDEZP');
+          `}
+        </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
